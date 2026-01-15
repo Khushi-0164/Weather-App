b8ed286f931630ad108449e77163418619e7e51b
Khushi Patidar <khushii@Khushis-MacBook-Air.local>
2026-01-15 20:46:37 +0530

    Fix main class package and rebuild

--- src/main/java/com/cfs/WeatherAppApplication.java
+++ src/main/java/com/cfs/WeatherAppApplication.java
@@ -1,4 +1,4 @@
-package com.cfs.Weather_App;
+package com.cfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeatherAppApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(WeatherAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherAppApplication.class, args);
+    }
 
 }
+
